734e50fe8ca1bbec4aa17bd97213da949d9c6a40
Ivan Diaz <ivanid22@gmail.com>
2020-02-27 12:13:35 -0300

    Add UserInterface and methods: display_board and ask_move

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,20 @@
 #!/usr/bin/env ruby
 
-puts 'Hello World!'
+class UserInterface
+    def self.display_board(board)
+      board.each_with_index do |row, j|
+        row.each_with_index do |element, i|
+          print "#{element}"
+          print '|' if i < row.length-1
+        end
+        print "\n-----\n" if j < board.length-1
+      end
+      print "\n"
+    end
+  
+    def self.ask_move(player)
+      puts "Enter move for player #{player[:id]}"
+      return gets.chomp
+    end
+  
+end
